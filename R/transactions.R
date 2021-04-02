c66177caf0d040e6d97d155065d88fbca870732a
willwooten <wwootencfc@gmail.com>
2021-04-02 16:36:54 -0500

    add tx_merkle_electrum

--- R/transactions.R
+++ R/transactions.R
@@ -18,5 +18,39 @@ tx_confirmation_status <- function(txid){
 tx_hex <- function(txid){
   url <- paste0("https://mempool.space/api/tx/", txid, "/hex")
   readLines(url, warn = FALSE)
-  
+}
+
+## Returns a transaction as binary data.
+
+tx_binary <- function(txid, destfile){
+  url <- paste0("https://mempool.space/api/tx/", txid, "/raw")
+  download.file(url, destfile)
+}
+
+## Returns a merkle inclusion proof for the transaction 
+## using bitcoind's merkleblock format.
+
+tx_merkle_bitcoind <- function(txid){
+  url <- paste0("https://mempool.space/api/tx/", txid, "/merkleblock-proof")
+  readLines(url, warn = FALSE)
+}
+
+
+## Returns a merkle inclusion proof for the transaction using 
+## Electrum's blockchain.transaction.get_merkle format.
+## A list of transaction hashes the current hash is paired with, recursively, 
+## in order to trace up to obtain merkle root of the block, deepest pairing first.
+## pos = The 0-based index of the position of the transaction 
+## in the ordered list of transactions in the block.
+
+
+tx_merkle_electrum <- function(txid){
+  url <- paste0("https://mempool.space/api/tx/", txid, "/merkle-proof")
+  df <- jsonlite::fromJSON(url)
+  data.frame(
+    txid = txid,
+    block_height = df$block_height, 
+    merkle = df$merkle, 
+    pos = df$pos
+  )
 }
