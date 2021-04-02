82b898fb0eb2414c58d32d76b67a75b5f8454739
willwooten <wwootencfc@gmail.com>
2021-04-02 13:53:07 -0500

    added notes

--- R/recommended_fees.R
+++ R/recommended_fees.R
@@ -1,3 +1,7 @@
+## Returns a dataframe of current recommended fee 
+## levels for fastest, half-hour and hour time frames
+## and also minimum fee currently being kept in 
+## the mempool
 
 recommended_fees <- function (){
   url <- "https://mempool.space/api/v1/fees/recommended"
