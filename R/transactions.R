49c882ed03ee9158a8675b50e7472712ec6b4241
willwooten <wwootencfc@gmail.com>
2021-04-02 16:18:14 -0500

    added tx_confirmation_status

---
+++ R/transactions.R
@@ -0,0 +1,14 @@
+## Returns the confirmation status of a transaction. 
+## Available fields: confirmed (boolean), block_height (optional)
+## and block_hash (optional).
+
+tx_confirmation_status <- function(txid){
+  url <- paste0("https://mempool.space/api/tx/", txid, "/status")
+  df <- jsonlite::fromJSON(url)
+  data.frame(
+    confirmed = df$confirmed, 
+    block_height = df$block_height, 
+    block_hash = df$block_hash,
+    block_time = df$block_time
+  )
+}
