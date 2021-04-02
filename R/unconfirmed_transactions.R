a4c740afdc01fc4a55d6f1b82f357cc573638a93
willwooten <wwootencfc@gmail.com>
2021-04-02 14:41:50 -0500

    count edit

--- R/unconfirmed_transactions.R
+++ R/unconfirmed_transactions.R
@@ -1,6 +1,6 @@
 ## Returns current count of unconfirmed transactions
 
-unconfirmed_transactions <- function(){
+unconfirmed_transactions_count <- function(){
   url <- "https://mempool.space/api/mempool"
   df <- fromJSON(url)
   df$count
